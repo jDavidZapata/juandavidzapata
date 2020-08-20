9db55a5f02c91b2ddae3d278f86c2b989e099a16
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-08-19 22:57:17 -0400

    Heroku config

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, session, abort, redirect, url_for, g, jsonify
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def home():
     error = None
     return render_template("index.html", error=error)
+
+if __name__ == '__main__':
+    app.run()
